cfeb50dc34a1a05437412a6105796baefb3ad8b9
Roman Kuzmin <nightroman@gmail.com>
2023-11-06 13:21:22 +0000

    Add BOM item instances

--- Nexar.Demo/Items/VariantBomItem.cs
+++ Nexar.Demo/Items/VariantBomItem.cs
@@ -1,6 +1,8 @@
 ﻿using MudBlazor;
 using Nexar.Client;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Nexar.Demo;
@@ -12,6 +14,7 @@ public sealed class VariantBomItem : LeafTreeItem
     }
 
     public IMyBom Tag { get; private set; }
+    public List<MyData> Items { get; private set; }
     public override string Text => "BOM";
     public override string Icon => Icons.Material.Filled.List;
     public new VariantItem Parent => (VariantItem)base.Parent;
@@ -35,5 +38,24 @@ public sealed class VariantBomItem : LeafTreeItem
         res.AssertNoErrors();
 
         Tag = res.Data.DesProjectById.Design.Variants[0].Bom;
+
+        Items = Tag?.BomItems
+            .Select(x => new MyData
+            {
+                Name = x.Component.Name,
+                IsManaged = x.Component.IsManaged,
+                Quantity = x.Quantity,
+                Instances = x.BomItemInstances.OrderBy(x => x.Designator).ToList()
+            })
+            .ToList();
+    }
+
+    public class MyData
+    {
+        public bool ShowDetails { get; set; }
+        public string Name { get; set; }
+        public bool IsManaged { get; set; }
+        public int Quantity { get; set; }
+        public IReadOnlyList<IMyBomItemInstance> Instances { get; set; }
     }
 }
